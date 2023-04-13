958f1e169a8b25e4d2e519b4ec8059ed8339ef5a
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2023-04-13 14:21:41 +0530

    multirazor Pay

---
+++ thirvusoft_bpm/thirvusoft_bpm/razorpay_backup/razorpay_checkout.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+from __future__ import unicode_literals
+
+import json
+
+from six import string_types
+
+import frappe
+from frappe import _
+from frappe.utils import cint, flt
+
+no_cache = 1
+
+expected_keys = (
+	"amount",
+	"title",
+	"description",
+	"reference_doctype",
+	"reference_docname",
+	"payer_name",
+	"payer_email",
+	"order_id",
+)
+
+
+def get_context(context):
+	context.no_cache = 1
+	try:
+		doc = frappe.get_doc("Integration Request", frappe.form_dict["token"])
+		details = json.loads(doc.data)
+		if(details['reference_doctype']=="Payment Request"):
+			pay_doc = frappe.db.get_value('Payment Request',details['reference_docname'],'reference_doctype')
+			if pay_doc == "Fees":
+				fee_doc = frappe.db.get_value('Payment Request',details['reference_docname'],'reference_name')
+				company = frappe.db.get_value('Fees',fee_doc,'company')
+		if company:
+			context.api_key = get_api_key(company)
+	except:
+		context.api_key = get_api_key()
+
+	try:
+		doc = frappe.get_doc("Integration Request", frappe.form_dict["token"])
+		payment_details = json.loads(doc.data)
+
+		for key in expected_keys:
+			context[key] = payment_details[key]
+
+		context["token"] = frappe.form_dict["token"]
+		context["amount"] = flt(context["amount"])
+		context["subscription_id"] = (
+			payment_details["subscription_id"] if payment_details.get("subscription_id") else ""
+		)
+
+	except Exception as e:
+		frappe.redirect_to_message(
+			_("Invalid Token"),
+			_("Seems token you are using is invalid!"),
+			http_status_code=400,
+			indicator_color="red",
+		)
+
+		frappe.local.flags.redirect_location = frappe.local.response.location
+		raise frappe.Redirect
+
+
+def get_api_key(company=None):
+	if company:
+		api_key = frappe.db.get_value("Company", company, "api_key")
+	else:
+		api_key = frappe.db.get_value("Razorpay Settings", None, "api_key")
+
+	if cint(frappe.form_dict.get("use_sandbox")):
+		api_key = frappe.conf.sandbox_api_key
+
+	return api_key
+
+
+@frappe.whitelist(allow_guest=True)
+def make_payment(razorpay_payment_id, options, reference_doctype, reference_docname, token):
+	data = {}
+
+	if isinstance(options, string_types):
+		data = json.loads(options)
+
+	data.update(
+		{
+			"razorpay_payment_id": razorpay_payment_id,
+			"reference_docname": reference_docname,
+			"reference_doctype": reference_doctype,
+			"token": token,
+		}
+	)
+
+	data = frappe.get_doc("Razorpay Settings").create_request(data)
+	frappe.db.commit()
+	return data
