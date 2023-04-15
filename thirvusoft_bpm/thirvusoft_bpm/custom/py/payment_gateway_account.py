f0caea4686813838ce7ae32cbb8ef1312b485572
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2023-04-15 16:05:08 +0530

    fix

--- thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_gateway_account.py
+++ thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_gateway_account.py
@@ -2,6 +2,5 @@ import frappe
 from erpnext.accounts.doctype.payment_gateway_account.payment_gateway_account import PaymentGatewayAccount
 class Autoname(PaymentGatewayAccount):
     def autoname(self):
-        company = frappe.get_value('Account',self.payment_account,'company')
-        abbr = frappe.get_value("Company",company,'abbr')
+        abbr = frappe.get_value("Company",self.company,'abbr')
         self.name = self.payment_gateway + " - " + abbr
