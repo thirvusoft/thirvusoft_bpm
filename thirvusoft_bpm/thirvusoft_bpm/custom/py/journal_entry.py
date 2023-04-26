ea19c30c418f2f48f0ec7d9b6beb0e1de98d8860
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2023-04-26 16:11:22 +0530

    chores:Journal Entry Automatic updation for Fees

---
+++ thirvusoft_bpm/thirvusoft_bpm/custom/py/journal_entry.py
@@ -0,0 +1,10 @@
+import frappe
+def update_fees(doc,event):
+    for acc in doc.accounts:
+        if acc.reference_type == "Fees" and acc.reference_name and acc.debit_in_account_currency > 0 and acc.credit_in_account_currency == 0 and acc.party_type == 'Student' and acc.party:
+            fees = frappe.get_doc('Fees',acc.reference_name)
+            fees.append('advance_payments',{
+                    'account':acc.account,
+                    'amount':acc.debit_in_account_currency
+                })
+            fees.save()
