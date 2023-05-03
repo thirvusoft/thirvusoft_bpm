8b0b662951699cbf5458415bf520ba1216d83892
Ubuntu <ubuntu@ip-172-31-27-209.ap-south-1.compute.internal>
2023-05-03 06:43:37 +0000

    fix:

--- thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_gateway_account.py
+++ thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_gateway_account.py
@@ -1,11 +1,11 @@
 import frappe
 from erpnext.accounts.doctype.payment_gateway_account.payment_gateway_account import PaymentGatewayAccount
 class Autoname(PaymentGatewayAccount):
-    def autoname(self):
-        abbr = frappe.get_value("Company",self.company,'abbr')
-        self.name = self.payment_gateway + " - " + abbr
-    
-    def validate(self):
+	def autoname(self):
+		abbr = frappe.get_value("Company",self.company,'abbr')
+		self.name = self.payment_gateway + " - " + abbr
+	
+	def validate(self):
 		self.currency = frappe.db.get_value("Account", self.payment_account, "account_currency")
 
 		# self.update_default_payment_gateway()
