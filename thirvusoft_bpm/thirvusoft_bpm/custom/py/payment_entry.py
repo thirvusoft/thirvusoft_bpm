e404acad4b344a65a115b5f098bedf83c4ed68c4
root <root@ip-172-31-25-247.ap-south-1.compute.internal>
2023-11-27 19:46:08 +0530

    fix

--- thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_entry.py
+++ thirvusoft_bpm/thirvusoft_bpm/custom/py/payment_entry.py
@@ -68,7 +68,8 @@ def send_message_confirmation(doc,event):
                 try:
                     if urls and i["phone_number"]:
                         mobile_number = i["phone_number"].replace("+", "")
-                        url = f'{ frappe.db.get_single_value('Whatsapp Settings','url')}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
+                        api_url = frappe.db.get_single_value('Whatsapp Settings','url')
+                        url = f'{api_url}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
                         payload={}
                         headers = {}
                         response = requests.request("GET", url, headers=headers, data=payload)
@@ -91,7 +92,8 @@ def send_message_confirmation(doc,event):
                 except Exception as e:
                     if urls and i["phone_number"]:
                         mobile_number = i["phone_number"].replace("+", "")
-                        url = f'{ frappe.db.get_single_value('Whatsapp Settings','url')}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
+                        api_url = frappe.db.get_single_value('Whatsapp Settings','url')
+                        url = f'{api_url)}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
                         payload={}
                         headers = {}
                         log_doc = frappe.new_doc("Whatsapp Log")
@@ -169,7 +171,8 @@ def send_purchase_msg(doc):
             try:
                 if urls and supplier_no:
                     mobile_number = supplier_no.replace("+", "")
-                    url = f'{ frappe.db.get_single_value('Whatsapp Settings','url')}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
+                    api_url = frappe.db.get_single_value('Whatsapp Settings','url')
+                    url = f'{api_url}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
                     payload={}
                     headers = {}
                     response = requests.request("GET", url, headers=headers, data=payload)
@@ -192,7 +195,8 @@ def send_purchase_msg(doc):
             except Exception as e:
                 if urls and supplier_no:
                     mobile_number = supplier_no.replace("+", "")
-                    url = f'{ frappe.db.get_single_value('Whatsapp Settings','url')}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
+                    api_url = frappe.db.get_single_value('Whatsapp Settings','url')
+                    url = f'{api_url}send?number=91{mobile_number}&type=media&message={def_v + encoded_s}&media_url={urls}&filename={pdf_name}&instance_id={instance_id}&access_token={access_token}'
                     payload={}
                     headers = {}
                     log_doc = frappe.new_doc("Whatsapp Log")
